5003f10358b5e8264357313c7d336063705e97da
ANDERSON ALVES DOS SANTOS <anderson.asantos55@senacsp.edu.br>
2024-05-14 19:28:08 -0300

    Primeiro Commit

---
+++ BotoesCalculadora/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace BotoesCalculadora
+{
+    partial class HotCalculadora
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HotCalculadora));
+            this.Menos = new System.Windows.Forms.Button();
+            this.Resultado = new System.Windows.Forms.TextBox();
+            this.Limpar = new System.Windows.Forms.Button();
+            this.Mais = new System.Windows.Forms.Button();
+            this.Multiplicar = new System.Windows.Forms.Button();
+            this.Divisao = new System.Windows.Forms.Button();
+            this.Igual = new System.Windows.Forms.Button();
+            this.Dois = new System.Windows.Forms.Button();
+            this.Um = new System.Windows.Forms.Button();
+            this.Tres = new System.Windows.Forms.Button();
+            this.Seis = new System.Windows.Forms.Button();
+            this.Cinco = new System.Windows.Forms.Button();
+            this.Quatro = new System.Windows.Forms.Button();
+            this.Novo = new System.Windows.Forms.Button();
+            this.Oito = new System.Windows.Forms.Button();
+            this.Sete = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Menos
+            // 
+            this.Menos.BackColor = System.Drawing.Color.White;
+            this.Menos.Font = new System.Drawing.Font("AIGDT", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.Menos.Location = new System.Drawing.Point(166, 90);
+            this.Menos.Name = "Menos";
+            this.Menos.Size = new System.Drawing.Size(93, 50);
+            this.Menos.TabIndex = 0;
+            this.Menos.Text = "-";
+            this.Menos.UseVisualStyleBackColor = false;
+            this.Menos.Click += new System.EventHandler(this.Menos_Click);
+            // 
+            // Resultado
+            // 
+            this.Resultado.Font = new System.Drawing.Font("AIGDT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.Resultado.Location = new System.Drawing.Point(166, 24);
+            this.Resultado.Name = "Resultado";
+            this.Resultado.Size = new System.Drawing.Size(344, 41);
+            this.Resultado.TabIndex = 1;
+            this.Resultado.TextChanged += new System.EventHandler(this.Resultado_TextChanged);
+            // 
+            // Limpar
+            // 
+            this.Limpar.Font = new System.Drawing.Font("AIGDT", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.Limpar.Location = new System.Drawing.Point(417, 90);
+            this.Limpar.Name = "Limpar";
+            this.Limpar.Size = new System.Drawing.Size(93, 50);
+            this.Limpar.TabIndex = 2;
+            this.Limpar.Text = "CE";
+            this.Limpar.UseVisualStyleBackColor = true;
+            this.Limpar.Click += new System.EventHandler(this.Limpar_Click);
+            // 
+            // Mais
+            // 
+            this.Mais.Font = new System.Drawing.Font("AIGDT", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.Mais.Location = new System.Drawing.Point(292, 92);
+            this.Mais.Name = "Mais";
+            this.Mais.Size = new System.Drawing.Size(93, 48);
+            this.Mais.TabIndex = 3;
+            this.Mais.Text = "+";
+            this.Mais.UseVisualStyleBackColor = true;
+            this.Mais.Click += new System.EventHandler(this.Mais_Click);
+            // 
+            // Multiplicar
+            // 
+            this.Multiplicar.Font = new System.Drawing.Font("AIGDT", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.Multiplicar.Location = new System.Drawing.Point(417, 158);
+            this.Multiplicar.Name = "Multiplicar";
+            this.Multiplicar.Size = new System.Drawing.Size(93, 48);
+            this.Multiplicar.TabIndex = 4;
+            this.Multiplicar.Text = "*";
+            this.Multiplicar.UseVisualStyleBackColor = true;
+            this.Multiplicar.Click += new System.EventHandler(this.Multiplicar_Click);
+            // 
+            // Divisao
+            // 
+            this.Divisao.Font = new System.Drawing.Font("Cooper Black", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Divisao.Location = new System.Drawing.Point(417, 226);
+            this.Divisao.Name = "Divisao";
+            this.Divisao.Size = new System.Drawing.Size(93, 47);
+            this.Divisao.TabIndex = 5;
+            this.Divisao.Text = "/";
+            this.Divisao.UseVisualStyleBackColor = true;
+            this.Divisao.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Igual
+            // 
+            this.Igual.Font = new System.Drawing.Font("AIGDT", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.Igual.Location = new System.Drawing.Point(417, 303);
+            this.Igual.Name = "Igual";
+            this.Igual.Size = new System.Drawing.Size(93, 46);
+            this.Igual.TabIndex = 6;
+            this.Igual.Text = "=";
+            this.Igual.UseVisualStyleBackColor = true;
+            this.Igual.Click += new System.EventHandler(this.Igual_Click);
+            // 
+            // Dois
+            // 
+            this.Dois.Font = new System.Drawing.Font("AIGDT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.Dois.Location = new System.Drawing.Point(247, 164);
+            this.Dois.Name = "Dois";
+            this.Dois.Size = new System.Drawing.Size(57, 37);
+            this.Dois.TabIndex = 7;
+            this.Dois.Text = "2";
+            this.Dois.UseVisualStyleBackColor = true;
+            // 
+            // Um
+            // 
+            this.Um.Font = new System.Drawing.Font("AIGDT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.Um.Location = new System.Drawing.Point(166, 164);
+            this.Um.Name = "Um";
+            this.Um.Size = new System.Drawing.Size(57, 37);
+            this.Um.TabIndex = 8;
+            this.Um.Text = "1";
+            this.Um.UseVisualStyleBackColor = true;
+            this.Um.Click += new System.EventHandler(this.Um_Click);
+            // 
+            // Tres
+            // 
+            this.Tres.Font = new System.Drawing.Font("AIGDT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.Tres.Location = new System.Drawing.Point(328, 164);
+            this.Tres.Name = "Tres";
+            this.Tres.Size = new System.Drawing.Size(57, 37);
+            this.Tres.TabIndex = 9;
+            this.Tres.Text = "3";
+            this.Tres.UseVisualStyleBackColor = true;
+            // 
+            // Seis
+            // 
+            this.Seis.Font = new System.Drawing.Font("AIGDT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.Seis.Location = new System.Drawing.Point(330, 238);
+            this.Seis.Name = "Seis";
+            this.Seis.Size = new System.Drawing.Size(56, 37);
+            this.Seis.TabIndex = 10;
+            this.Seis.Text = "6";
+            this.Seis.UseVisualStyleBackColor = true;
+            this.Seis.Click += new System.EventHandler(this.Seis_Click);
+            // 
+            // Cinco
+            // 
+            this.Cinco.Font = new System.Drawing.Font("AIGDT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.Cinco.Location = new System.Drawing.Point(248, 238);
+            this.Cinco.Name = "Cinco";
+            this.Cinco.Size = new System.Drawing.Size(57, 35);
+            this.Cinco.TabIndex = 11;
+            this.Cinco.Text = "5";
+            this.Cinco.UseVisualStyleBackColor = true;
+            this.Cinco.Click += new System.EventHandler(this.Cinco_Click);
+            // 
+            // Quatro
+            // 
+            this.Quatro.Font = new System.Drawing.Font("AIGDT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.Quatro.Location = new System.Drawing.Point(166, 239);
+            this.Quatro.Name = "Quatro";
+            this.Quatro.Size = new System.Drawing.Size(56, 36);
+            this.Quatro.TabIndex = 12;
+            this.Quatro.Text = "4";
+            this.Quatro.UseVisualStyleBackColor = true;
+            // 
+            // Novo
+            // 
+            this.Novo.Font = new System.Drawing.Font("AIGDT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.Novo.Location = new System.Drawing.Point(329, 313);
+            this.Novo.Name = "Novo";
+            this.Novo.Size = new System.Drawing.Size(57, 36);
+            this.Novo.TabIndex = 13;
+            this.Novo.Text = "9";
+            this.Novo.UseVisualStyleBackColor = true;
+            this.Novo.Click += new System.EventHandler(this.Novo_Click);
+            // 
+            // Oito
+            // 
+            this.Oito.Font = new System.Drawing.Font("AIGDT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.Oito.Location = new System.Drawing.Point(248, 314);
+            this.Oito.Name = "Oito";
+            this.Oito.Size = new System.Drawing.Size(56, 35);
+            this.Oito.TabIndex = 14;
+            this.Oito.Text = "8";
+            this.Oito.UseVisualStyleBackColor = true;
+            this.Oito.Click += new System.EventHandler(this.Oito_Click);
+            // 
+            // Sete
+            // 
+            this.Sete.Font = new System.Drawing.Font("AIGDT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.Sete.Location = new System.Drawing.Point(168, 315);
+            this.Sete.Name = "Sete";
+            this.Sete.Size = new System.Drawing.Size(55, 34);
+            this.Sete.TabIndex = 15;
+            this.Sete.Text = "7";
+            this.Sete.UseVisualStyleBackColor = true;
+            this.Sete.Click += new System.EventHandler(this.Sete_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("AIGDT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.button1.Location = new System.Drawing.Point(168, 381);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(218, 31);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "0";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // HotCalculadora
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::BotoesCalculadora.Properties.Resources.furious_hot_wheels_by_algorithmiccreative_dg2i2fd_fullview;
+            this.ClientSize = new System.Drawing.Size(710, 448);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Sete);
+            this.Controls.Add(this.Oito);
+            this.Controls.Add(this.Novo);
+            this.Controls.Add(this.Quatro);
+            this.Controls.Add(this.Cinco);
+            this.Controls.Add(this.Seis);
+            this.Controls.Add(this.Tres);
+            this.Controls.Add(this.Um);
+            this.Controls.Add(this.Dois);
+            this.Controls.Add(this.Igual);
+            this.Controls.Add(this.Divisao);
+            this.Controls.Add(this.Multiplicar);
+            this.Controls.Add(this.Mais);
+            this.Controls.Add(this.Limpar);
+            this.Controls.Add(this.Resultado);
+            this.Controls.Add(this.Menos);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(726, 487);
+            this.MinimumSize = new System.Drawing.Size(726, 487);
+            this.Name = "HotCalculadora";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "HotCalculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Menos;
+        private System.Windows.Forms.TextBox Resultado;
+        private System.Windows.Forms.Button Limpar;
+        private System.Windows.Forms.Button Mais;
+        private System.Windows.Forms.Button Multiplicar;
+        private System.Windows.Forms.Button Divisao;
+        private System.Windows.Forms.Button Igual;
+        private System.Windows.Forms.Button Dois;
+        private System.Windows.Forms.Button Um;
+        private System.Windows.Forms.Button Tres;
+        private System.Windows.Forms.Button Seis;
+        private System.Windows.Forms.Button Cinco;
+        private System.Windows.Forms.Button Quatro;
+        private System.Windows.Forms.Button Novo;
+        private System.Windows.Forms.Button Oito;
+        private System.Windows.Forms.Button Sete;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ BotoesCalculadora/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BotoesCalculadora
+{
+    public partial class HotCalculadora : Form
+    {
+        public HotCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Menos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Mais_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Limpar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Multiplicar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Igual_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Um_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Cinco_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Seis_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Novo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Oito_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Sete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Resultado_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
